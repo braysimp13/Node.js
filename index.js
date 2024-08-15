066e754ec84e03cfe5d2a5e45ac190002ce1534f
Brayden Simpson <Bms0072@auburn.edu>
2024-08-14 21:42:21 -0400

    Testing Event Emitter

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('testing!');
+const logEvents = require('./logEvent');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+//initialize object
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+    myEmitter.emit('log', 'Log event emitted!\n');
+}, 2000
+)
